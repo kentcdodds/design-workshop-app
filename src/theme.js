84e9b4a2651194cf1348806328deb659d3f7f82d
Vojta Holik <vojta@egghead.io>
2020-03-06 14:57:08 +0100

    apply styles

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#fff',
+  text: '#212b36',
+  textLightest: '#8E9EAC',
+  primary: '#1675ff',
+  sky: '#E9EDF1',
+  skyLight: '#F4F6F8',
+  skyDark: '#C4CDD5',
+}
